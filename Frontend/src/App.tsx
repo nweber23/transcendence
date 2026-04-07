715600ec2eea669cab3f8715ae03cf38b4872702
Niklas Weber <nweber@student.42Heilbronn.de>
2026-04-07 12:59:19 +0200

    add: include Poker game route in the application

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Landing from '@/pages/landing/Landing';
 import Login from '@/pages/auth/Login';
 import SignUp from '@/pages/auth/SignUp';
 import Blackjack from '@/pages/games/Blackjack';
+import Poker from '@/pages/games/Poker';
 import '@/styles/globals.css';
 
 const App: React.FC = () => {
@@ -18,6 +19,7 @@ const App: React.FC = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/games/blackjack" element={<Blackjack />} />
+          <Route path="/games/poker" element={<Poker />} />
         </Routes>
         <Footer />
       </div>
