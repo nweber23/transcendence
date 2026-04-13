85b040886db0b5f4e81cbbd66d7e9fe35a320fc5
Niklas Weber <nweber@student.42Heilbronn.de>
2026-04-13 14:12:02 +0200

    fix: update dependencies and correct rotation calculation in Beams component

--- Frontend/src/components/ui/Beams.tsx
+++ Frontend/src/components/ui/Beams.tsx
@@ -2,7 +2,6 @@ import { forwardRef, useImperativeHandle, useEffect, useRef, useMemo, FC, ReactN
 import * as THREE from 'three';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
-import { degToRad } from 'three/src/math/MathUtils.js';
 
 type UniformValue = THREE.IUniform<unknown> | unknown;
 
@@ -220,7 +219,7 @@ const Beams: FC<BeamsProps> = ({
 
   return (
     <CanvasWrapper>
-      <group rotation={[0, 0, degToRad(rotation)]}>
+      <group rotation={[0, 0, THREE.MathUtils.degToRad(rotation)]}>
         <PlaneNoise ref={meshRef} material={beamMaterial} count={beamNumber} width={beamWidth} height={beamHeight} />
         <DirLight color={lightColor} position={[0, 3, 10]} />
       </group>
