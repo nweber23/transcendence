2b50d37ded7cfc584ef277b1d1dcc42ce77babe0
qFelix <>
2026-05-16 14:25:12 +0200

    Basic user creation INTEGRATION tests

--- Backend/services/services_test.go
+++ Backend/services/services_test.go
@@ -1,19 +1,29 @@
 package services
 
 import (
+	"fmt"
+
 	"testing"
-	"github.com/DATA-DOG/go-sqlmock"
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"transcendence/models"
 )
 
 func InitMockDB(testInterface *testing.T) (*gorm.DB) {
-	mock_db, _, _ := sqlmock.New()
-	mock_dialector := postgres.New(postgres.Config{
-		Conn:       mock_db,
-		DriverName: "postgres",
-	})
-	db, _ := gorm.Open(mock_dialector, &gorm.Config{})
+	db, _ := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err := db.AutoMigrate(
+		&models.User{},
+		&models.Account{},
+		&models.Transaction{},
+		&models.Game{},
+		&models.BlackjackGame{},
+		&models.PokerGame{},
+		&models.SlotsGame{},
+		&models.GameStatistics{},
+	); err != nil {
+		fmt.Errorf(`failed to run migrations: %w`, err)
+		testInterface.FailNow()
+	}
 	return db
 }
 
@@ -36,8 +46,31 @@ func createMockUsers(testInterface *testing.T, userService *UserService) {
 	}
 }
 
+// GetUserByName retrieves a user by username
+func (s *UserService) tbr_GetUserByName(username string) (*models.User, error) {
+	var user models.User
+	if err := s.db.Where("username = ? AND deleted_at IS NULL", username).First(&user).Error; err != nil {
+		return nil, fmt.Errorf("user not found: %w", err)
+	}
+	return &user, nil
+}
+
+func fetchMockUsers(testInterface *testing.T, userService *UserService) {
+	index := '5'
+	for index > '0' {
+		index--
+		testName := "test" + string(index)
+		_, err := userService.tbr_GetUserByName(testName)
+		if err != nil {
+			testInterface.Errorf(`User %s was expected to be in the database but wasn't`, testName)
+		}
+	}
+}
+
 func TestAccountService(testInterface *testing.T) {
 	accountService, userService := createMockServices(testInterface)
-	createMockUsers(testInterface, userService)
 	_ = accountService
+
+	createMockUsers(testInterface, userService)
+	fetchMockUsers(testInterface, userService)
 }
