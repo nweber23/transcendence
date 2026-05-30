dc59bb43e9f22e4c805cb7602a693be4f3321f20
flotschilein <florianbraune1811@gmail.com>
2026-05-30 18:33:41 +0200

    added comment

---
+++ Engine/games/cardGames/shared/includes/deck.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdexcept>
+#include "card.hpp"
+
+namespace deck {
+
+// --------------------------------------------------
+// Deck
+// --------------------------------------------------
+
+class Deck {
+private:
+    std::vector<Card> _cards;
+    std::size_t _nextCard = 0;
+
+public:
+    explicit Deck(std::size_t numDecks = 1);
+
+    Card pick();
+    void reshuffle();
+};
+
+}
