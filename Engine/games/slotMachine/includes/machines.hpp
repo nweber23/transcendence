f79d2a2ca50d5afa4c0b25f7e362de2ba7ef80b3
flotschilein <florianbraune1811@gmail.com>
2026-06-07 18:26:58 +0200

    working on the logic of the , class

--- Engine/games/slotMachine/includes/machines.hpp
+++ Engine/games/slotMachine/includes/machines.hpp
@@ -4,6 +4,12 @@
 #include <cstdint>
 #include <thread>
 
+enum class volatility_t : uint8_t {
+    low,
+    medium,
+    high
+};
+
 class Machine {
     private:
         std::atomic<std::uint64_t> idx;
@@ -11,4 +17,6 @@ class Machine {
     public:
         Machine();
         ~Machine();
+        [[nodiscard]]
+        std::uint32_t get_monetary_result(volatility_t vol, std::uint8_t rtp) const;
 };
--- Engine/games/slotMachine/src/machines.cpp
+++ Engine/games/slotMachine/src/machines.cpp
@@ -1,10 +1,12 @@
-#include "../machines.hpp"
+#include "../includes/rtp_exception.hpp"
+#include "../includes/machines.hpp"
 
 Machine::Machine() {
     std::random_device rd;
     std::mt19937 gen(rd());
     idx = static_cast<uint64_t>(gen());
-    if (counter_thread.joinable()) return;
+    if (counter_thread.joinable())
+        return;
     counter_thread = std::jthread([](std::stop_token st, std::atomic<std::uint64_t>& i) {
         while (!st.stop_requested()) {
             ++i;
@@ -15,4 +17,15 @@ Machine::Machine() {
 Machine::~Machine() {
     counter_thread.request_stop();
     counter_thread.join();
+}
+
+std::uint32_t Machine::get_monetary_result(volatility_t vol, std::uint8_t rtp) const {
+    if (rtp > 100) {
+        throw rtp_too_high(rtp);
+    }
+    switch (vol)
+        case volatility_t::low:
+        case volatility_t::medium:
+        case volatility_t::high:
+    return 0;
 }
