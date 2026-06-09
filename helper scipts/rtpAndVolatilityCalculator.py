18f89bf2d054a4eec7c083e01909875c30bd9021
flotschilein <florianbraune1811@gmail.com>
2026-06-09 23:52:53 +0200

    calc posibilities

--- helper scipts/rtpAndVolatilityCalculator.py
+++ helper scipts/rtpAndVolatilityCalculator.py
@@ -68,10 +68,10 @@ print("Scatter Paytable:")
 print(json.dumps(scatterpaytable, indent=2))
 print("=============================================")
 
+combination_count = math.prod(len(reel) for reel in reels)
 for active_lines in lineoptions:
     print(f"Calculations for {active_lines}")
     standard_gain = 0
     scatter_gain = 0
     free_spin_gain = 0
-    combination_count = math.prod(len(reel) for reel in reels)
     print(f"count{combination_count}")
