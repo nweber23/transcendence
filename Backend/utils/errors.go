f5c2f9fe348dbad968820f9fb4244d215275a026
qFelix <102669377+qFelixDev@users.noreply.github.com>
2026-06-16 17:48:04 +0200

    store error instances

---
+++ Backend/utils/errors.go
@@ -0,0 +1,20 @@
+package utils
+
+var (
+	ErrInvalidFilename           = errors.New("invalid filename")
+	ErrRandomStringGenFailed     = errors.New("failed to generate random string")
+	ErrNegativeTransactionAmount = errors.New("amount must be greater than zero")
+	ErrInsufficientBalance       = errors.New("insufficient balance")
+	ErrSelfLove                  = errors.New("self love only works irl")
+	ErrAlreadyBefriended         = errors.New("friend already added")
+	ErrAlreadyDefriended         = errors.New("no friend to remove")
+	ErrInvalidFriendshipStatus   = errors.New("invalid friendship status")
+	ErrAccountNotFound           = errors.New("account not found")
+	ErrGameNotFound              = errors.New("game not found")
+	ErrUsernameWrongLength       = errors.New("username must be between 3 and 32 characters")
+	ErrInvalidUsername           = errors.New("invalid username")
+	ErrInvalidPassword           = errors.New("invalid password")
+	ErrInvalidEmail              = errors.New("invalid email")
+	ErrInvalidToken              = errors.New("invalid token")
+	ErrEntryExists               = errors.New("matching entry already exists")
+)
