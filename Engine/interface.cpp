353f6349aaa405de019ea6c5b80654ad29d7f4a0
flotschilein <florianbraune1811@gmail.com>
2026-06-16 16:59:21 +0200

    put the logic in the interface.cpp for now so i can run blackjack

--- Engine/interface.cpp
+++ Engine/interface.cpp
@@ -16,3 +16,48 @@ std::string Engine::run_slot(std::string_view game_name,
                              std::uint32_t bet_per_line) {
     return slots.run_slot(game_name, line_count, bet_per_line);
 }
+
+bool Engine::blackjack_exists(std::string_view game_id) const {
+    return blackjack_games.contains(std::string(game_id));
+}
+
+std::string Engine::create_blackjack(std::string_view game_id, std::int64_t bet) {
+    auto key = std::string(game_id);
+    if (blackjack_games.contains(key)) {
+        return {};
+    }
+    auto [it, inserted] = blackjack_games.emplace(key, blackjack::Game{});
+    try {
+        it->second.deal(bet);
+    } catch (...) {
+        blackjack_games.erase(it);
+        return {};
+    }
+    return blackjack::serialize_game_state(it->second);
+}
+
+std::string Engine::blackjack_hit(std::string_view game_id) {
+    auto it = blackjack_games.find(std::string(game_id));
+    if (it == blackjack_games.end()) {
+        return {};
+    }
+    try {
+        it->second.hit();
+    } catch (...) {
+        return {};
+    }
+    return blackjack::serialize_game_state(it->second);
+}
+
+std::string Engine::blackjack_stand(std::string_view game_id) {
+    auto it = blackjack_games.find(std::string(game_id));
+    if (it == blackjack_games.end()) {
+        return {};
+    }
+    try {
+        it->second.stand();
+    } catch (...) {
+        return {};
+    }
+    return blackjack::serialize_game_state(it->second);
+}
--- Engine/interface.hpp
+++ Engine/interface.hpp
@@ -1,11 +1,15 @@
 #pragma once
 #include "games/slotMachine/includes/machines.hpp"
+#include "games/cardGames/blackJack/includes/engine.hpp"
 #include <cstdint>
 #include <string>
+#include <unordered_map>
 
 class Engine {
 private:
     Machine slots;
+    std::unordered_map<std::string, blackjack::Game> blackjack_games;
+
 public:
     Engine() = default;
 
@@ -16,4 +20,16 @@ public:
     std::string run_slot(std::string_view game_name,
                          std::uint8_t line_count,
                          std::uint32_t bet_per_line);
+
+    [[nodiscard]]
+    bool blackjack_exists(std::string_view game_id) const;
+
+    [[nodiscard]]
+    std::string create_blackjack(std::string_view game_id, std::int64_t bet);
+
+    [[nodiscard]]
+    std::string blackjack_hit(std::string_view game_id);
+
+    [[nodiscard]]
+    std::string blackjack_stand(std::string_view game_id);
 };
