338d09c2b9a98d6c6bd9839e90766d07854146ce
qFelix <gabrielhodges2006@yahoo.com>
2026-07-14 15:00:38 +0200

    Add more protogen code

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-mkdir -p grpc
-protoc --go_out=grpc --go_opt=paths=source_relative engine.proto
+mkdir -p engine_proto
+protoc \
+	--go_out=engine_proto      --go_opt=paths=source_relative \
+	--go-grpc_out=engine_proto --go-grpc_opt=paths=source_relative \
+engine.proto
 go build -o backend main.go
