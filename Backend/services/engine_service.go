97639872d480f260e9f07a956add8110cfa4e4f8
qFelix <gabrielhodges2006@yahoo.com>
2026-07-14 17:04:23 +0200

    current state of engine service

--- Backend/services/engine_service.go
+++ Backend/services/engine_service.go
@@ -12,6 +12,11 @@ type EngineService struct {
 	client     engine_proto.GameEngineClient
 }
 
+type EngineGame struct {
+	service *EngineService
+	game_id uint
+}
+
 func NewEngineService(engineURL string, enginePort string) (*EngineService, error) {
 	options := []grpc.DialOption{
 		// TODO: Proper TLS credentials
@@ -30,4 +35,22 @@ func NewEngineService(engineURL string, enginePort string) (*EngineService, erro
 
 func (engineService *EngineService) Remove() {
 	engineService.connection.Close()
+}
+
+type TexasGame EngineGame
+
+type TexasGameState struct {
+	
+}
+
+func (engineService *EngineService) CreateTexasGame(numPlayers uint32, amount int64) (*TexasGame, error) {
+	request := engine_proto.TexasRequest{
+		Action:     "create",
+		Amount:     amount,
+		NumPlayers: numPlayers,
+	}
+	response, err := engineService.client.TexasAction(context.Background(), &request)
+	if err != nil {
+		return nil, err
+	}
 }
