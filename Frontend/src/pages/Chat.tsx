b99a2aa8def2d8c42ad79bb2f7ce0104dbfd12c3
qFelix <gabrielhodges2006@yahoo.com>
2026-08-12 09:03:00 +0200

    Beign chat tsx

---
+++ Frontend/src/pages/Chat.tsx
@@ -0,0 +1,11 @@
+const Chat: React.FC = () => {
+  return (
+  	<div className="w-full max-w-full flex justify-center items-center">
+      <div className="w-[200px] h-[50px] border border-[rgba(212,175,55,0.4)] ml-[0px]">
+        Chat example page
+      </div>
+    </div>
+  )
+}
+
+export default Chat;
